19067c30ba4e9f79704f23737925b0b536188c1b
phongleland <phongleland@Phongs-iMac.local>
2016-03-20 19:07:19 -0700

    Consumer endpoint.  Create action

--- app/controllers/api/v1/consumers_controller.rb
+++ app/controllers/api/v1/consumers_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::ConsumersController < ApplicationController
   def show
     respond_with Consumer.find(params[:id])
   end
+  
+  def create
+    consumer = Consumer.new(consumer_params)
+    if consumer.save
+      render json: consumer, status: 201, location: [:api, consumer]
+    else
+      render json: { errors: consumer.errors }, status: 422
+    end
+  end
+
+  private
+
+    def consumer_params
+      params.require(:consumer).permit(:lastname, :firstname)
+    end
 end
--- spec/controllers/api/v1/consumers_controller_spec.rb
+++ spec/controllers/api/v1/consumers_controller_spec.rb
@@ -34,4 +34,39 @@ RSpec.describe Api::V1::ConsumersController, type: :controller do
     it { should respond_with 200 }
   end
   
+  describe "POST #create" do
+    context "when successful" do
+      before(:each) do
+        @valid_attributes = FactoryGirl.attributes_for :consumer
+        post :create, { consumer: @valid_attributes }
+      end
+
+      it "renders the json representation" do
+        consumer_response = JSON.parse(response.body, symbolize_names: true)
+        expect(consumer_response[:firstname]).to eql @valid_attributes[:firstname]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when failed" do
+      before(:each) do
+        @invalid_attributes = { firstname: "John", lastname: nil }
+        post :create, { consumer: @invalid_attributes }
+      end
+
+      it "renders the json errors" do
+        consumer_response = JSON.parse(response.body, symbolize_names: true)
+        expect(consumer_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the consumer could not be created" do
+        consumer_response = JSON.parse(response.body, symbolize_names: true)
+        expect(consumer_response[:errors][:lastname]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+  
 end
